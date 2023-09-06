d7a23d56f9b057530830230467ddcc3fb974c4b1
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-09-06 19:12:32 +0200

    initial

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Simple Quiz game
+
+This is a simple Quiz application built with HTML, CSS, and JavaScript.
+
+
+![App Screenshot](screenshots/quiz.png)
+
+
---
+++ script.js
@@ -0,0 +1,147 @@
+const questions = [{
+        question: "What is the language flutter uses ?",
+        answers: [
+            { text: "Dart", correct: true },
+            { text: "Java", correct: false },
+            { text: "Python", correct: false },
+            { text: "C++", correct: false }
+        ]
+    },
+    {
+        question: "What gas do plants absorb during photosynthesis?",
+        answers: [
+            { text: "Oxygen", correct: false },
+            { text: "Nitrogen", correct: false },
+            { text: "Hydrogen", correct: false },
+            { text: "Carbon dioxide", correct: true }
+        ]
+    },
+
+    {
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        answers: [
+
+            { text: "Charles Dickens", correct: false },
+            { text: "Jane Austen", correct: false },
+            { text: "William Shakespeare", correct: true },
+            { text: "F. Scott Fitzgerald", correct: false }
+        ]
+    },
+
+    {
+        question: "What is the capital of France?",
+        answers: [
+
+            { text: "London", correct: false },
+            { text: "Berlin", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Madrid", correct: false }
+        ]
+    },
+
+    {
+        question: "Which planet is known as the 'Red Planet'?",
+        answers: [
+            { text: "Mars", correct: true },
+            { text: "Venus", correct: false },
+            { text: "Jupiter", correct: false },
+            { text: "Saturn", correct: false }
+        ]
+    },
+
+    {
+        question: "What is the chemical symbol for gold?",
+        answers: [
+            { text: "Ag", correct: false },
+            { text: "Fe", correct: false },
+            { text: "Au", correct: true },
+            { text: "Cu", correct: false }
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    // for (let i = 0; i < currentQuestion.answers.length; i++) {
+    //     let button = document.createElement('button');
+    //     button.classList.add('btn');
+    //     button.innerHTML = currentQuestion.answers[i]['text'];
+    //     answerButtonsElement.appendChild(button);
+    // }
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.classList.add('btn');
+        button.innerHTML = answer.text;
+        answerButtonsElement.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+    });
+
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add('correct');
+        score++;
+    } else {
+        selectedBtn.classList.add('incorrect');
+    }
+    Array.from(answerButtonsElement.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add('correct');
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    questionElement.innerHTML = "Your Score is " + score + " out of " + questions.length;
+    answerButtonsElement.innerHTML = "";
+    nextButton.innerHTML = "Restart";
+    nextButton.style.display = "block";
+    nextButton.addEventListener('click', startQuiz);
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+nextButton.addEventListener('click', () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else { showScore(); }
+});
+startQuiz();
